537529b29b926a20e78b360818268cb1eb0d96b9
Henry Post <HenryFBP@gmail.com>
2019-02-05 16:37:25 -0600

    add utility for viewing web pages via terminal

--- vagrant-config/scripts/setup-general.sh
+++ vagrant-config/scripts/setup-general.sh
@@ -5,6 +5,9 @@ echo "Doing general setup."
 # Install add-apt-repository command.
 apt-get install -y software-properties-common
 
+# Install terminal web browser.
+apt-get install -y lynx
+
 # Add Python repo.
 add-apt-repository ppa:deadsnakes/ppa
 
