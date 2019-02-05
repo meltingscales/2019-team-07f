a5c35cdec4cd59556c87f52d48f6ac493dc5f58e
HenryFBP <HenryFBP@gmail.com>
2019-02-05 12:08:01 -0600

    fix manual build script not starting tomcat

--- server/build.bat
+++ server/build.bat
@@ -1,6 +1,6 @@
-mvn clean install
+REM mvn clean install
 
 REM Starts Tomcat but silently exits if it is already running.
-START /MIN CMD.EXE /C %CATALINA_HOME%/bin/startup.bat
+CMD.EXE /C "%CATALINA_HOME%\bin\startup.bat"
 
 mvn tomcat7:redeploy
