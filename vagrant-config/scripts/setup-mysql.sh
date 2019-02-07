07e202a0750f2a5744b159b8d843d1a0befc73f0
Henry Post <HenryFBP@gmail.com>
2019-02-07 12:16:28 -0600

    use password as password instead of root

--- vagrant-config/scripts/setup-mysql.sh
+++ vagrant-config/scripts/setup-mysql.sh
@@ -1,17 +1,17 @@
 
 # Empty password.
 # This also prevents any prompts from coming up.
-echo "mysql-server-5.7 mysql-server/root_password password root"        | sudo debconf-set-selections
-echo "mysql-server-5.7 mysql-server/root_password_again password root"  | sudo debconf-set-selections
+echo "mysql-server-5.7 mysql-server/root_password password password"        | sudo debconf-set-selections
+echo "mysql-server-5.7 mysql-server/root_password_again password password"  | sudo debconf-set-selections
 
 apt-get install -y mysql-server-5.7 mysql-client
 
 service mysql start
 
 # Run our setup script.
-mysql -uroot < "/vagrant/server/src/main/resources/setup.sql"
+mysql -uroot -ppassword < "/vagrant/server/src/main/resources/setup.sql"
 
 # Setup our blank password.
-mysql -uroot -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password by 'password'; FLUSH PRIVILEGES;"
+#mysql -uroot -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password by 'password'; FLUSH PRIVILEGES;"
 
 service mysql restart
