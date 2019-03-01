080e3d05513ff70fe36a58ae4fb487797fad1df4
jtron82 <tranj46@gmail.com>
2019-03-01 13:52:09 -0600

    working hardcoded authentication

--- server/src/main/java/com/teamteem/dao/Login.java
+++ server/src/main/java/com/teamteem/dao/Login.java
@@ -12,87 +12,87 @@ import java.io.Serializable;
 @SessionScoped
 public class Login implements Serializable {
 
-  private static final long serialVersionUID = -1776415679978131804L;
+    private static final long serialVersionUID = -1776415679978131804L;
 
-  private String username;
-  private String password;
+    private String username;
+    private String password;
 
-  private User currentUser;
+    private User currentUser;
 
-  //private SessionFactory sessionFactory;
+    //private SessionFactory sessionFactory;
 
-  public String login() {
-    currentUser = validate(username, password);
+    public String login() {
+        currentUser = validate(username, password);
 
-    if (currentUser != null) {
-      return "/logged_in/home.xhtml?faces-redirect=true";
-    } else {
-      FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Incorrect Username or Password", "Invalid Credentials"));
-      return null;
-    }
-  }
-
-  // invalidate/logout the session
-  public String logout() {
-    FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
-    return "/login.xhtml?faces-redirect=true";
-  }
-
-  // check if current user is logged in
-  public boolean isLoggedIn() {
-    return currentUser != null;
-  }
-
-  /*public boolean validate(String username, String password) {
-    Session session = sessionFactory.getCurrentSession();
-    Query query = session.createQuery("FROM Person WHERE username = ? AND password = ?").setParameter(0, person.getUsername(), person.getPassword());
-
-    if (query.getResultList().size() > 0) {
-      return true;
-    } else {
-      return false;
-    }
-  }*/
-
-   private User validate(String username, String password) {
-    User result = null;
-
-    if (username.equals("admin") && password.equals("admin")) {
-      result = new User(username, "Jimmy Tran");
+        if (currentUser != null) {
+            return "/logged_in/home.xhtml?faces-redirect=true";
+        } else {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Incorrect Username or Password", "Invalid Credentials"));
+            return null;
+        }
     }
 
-    /*try {
-        SessionFactory sf = new Configuration().configure().buildSessionFactory();
-        Session session = sf.openSession();
-        session.beginTransaction();
-        Query query = session.createQuery("FROM person WHERE username = ? AND password = ?");
-        query.setString("username", "username");
-        query.setString("password", "password");
-    } catch (Exception e) {
-        e.printStackTrace();
+    // invalidate/logout the session
+    public String logout() {
+        FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+        return "/login.xhtml?faces-redirect=true";
+    }
+
+    // check if current user is logged in
+    public boolean isLoggedIn() {
+        return currentUser != null;
+    }
+
+    /*public boolean validate(String username, String password) {
+        Session session = sessionFactory.getCurrentSession();
+        Query query = session.createQuery("FROM Person WHERE username = ? AND password = ?").setParameter(0, person.getUsername(), person.getPassword());
+
+        if (query.getResultList().size() > 0) {
+          return true;
+        } else {
+          return false;
+        }
     }*/
 
-    return result;
-  }
+    private User validate(String username, String password) {
+        User result = null;
 
-  public String getUsername() {
-    return username;
-  }
+        if (username.equals("admin") && password.equals("admin")) {
+            result = new User(username, "Hardcoded User");
+        }
 
-  public void setUsername(String username) {
-    this.username = username;
-  }
+        /*try {
+            SessionFactory sf = new Configuration().configure().buildSessionFactory();
+            Session session = sf.openSession();
+            session.beginTransaction();
+            Query query = session.createQuery("FROM person WHERE username = ? AND password = ?");
+            query.setString("username", "username");
+            query.setString("password", "password");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }*/
 
-  public String getPassword() {
-    return password;
-  }
+            return result;
+        }
 
-  public void setPassword(String password) {
-    this.password = password;
-  }
+    public String getUsername() {
+        return username;
+    }
 
-  public User getCurrentUser() {
-    return currentUser;
-  }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public User getCurrentUser() {
+        return currentUser;
+    }
 
 }
