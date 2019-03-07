9fea1001a69e246fe8d9d9800b474c97964f1c41
HenryFBP <HenryFBP@gmail.com>
2019-03-06 22:04:32 -0600

    remove constructor which breaks build

--- server/src/main/java/com/teamteem/model/Person.java
+++ server/src/main/java/com/teamteem/model/Person.java
@@ -8,17 +8,6 @@ import javax.persistence.*;
 @ManagedBean(name = "person")
 public class Person {
 
-    public Person() {
-    }
-
-    public Person(String name, String username, String email, String country, String password) {
-        this.name = name;
-        this.username = username;
-        this.email = email;
-        this.country = country;
-        this.password = password;
-    }
-
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
