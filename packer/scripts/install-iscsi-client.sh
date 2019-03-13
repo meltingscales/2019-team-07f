5ea626056d53709122c5c74fd28d970079ece96d
Henry Post <HenryFBP@gmail.com>
2019-03-13 13:43:18 -0500

    add iscsi (still stubs!) setup scripts to webserver and storage packer boxes

--- packer/scripts/install-iscsi-client.sh
+++ packer/scripts/install-iscsi-client.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+
+echo "TODO!!!"
+
 echo https://help.ubuntu.com/lts/serverguide/iscsi-initiator.html.en
 echo https://www.server-world.info/en/note?os=Ubuntu_18.04&p=iscsi&f=2
