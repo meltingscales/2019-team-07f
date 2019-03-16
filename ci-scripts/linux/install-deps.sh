7bef198ff1a460ff92d96bd24347fe592152faa2
Henry Post <HenryFBP@gmail.com>
2019-03-16 00:46:46 -0500

    extract linux deps to own sh file

---
+++ ci-scripts/linux/install-deps.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Install normal deps
+sudo apt -y install ruby
+
+# Purge existing vbox for proper installation
+sudo apt autoremove virtualbox-dkms
+
+# Install VirtualBox.
+sudo apt -y install build-essential linux-headers-`uname -r` dkms virtualbox-dkms
+
+# Install Vagrant.
+sudo apt -y install vagrant virtualbox
+
+# Pre-accept virtualbox-ext-pack license.
+echo virtualbox-ext-pack virtualbox-ext-pack/license select true | sudo debconf-set-selections
+
+# Install virtualbox-ext-pack
+sudo apt -y install virtualbox-ext-pack
