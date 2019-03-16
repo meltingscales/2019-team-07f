cdb45a6345e02f373b4c82441839e6cbbd655b41
Henry Post <HenryFBP@gmail.com>
2019-03-16 10:01:07 -0500

    add travis install steps

--- ci-scripts/linux/install-deps.sh
+++ ci-scripts/linux/install-deps.sh
@@ -6,11 +6,14 @@ sudo apt -y install ruby
 # Purge existing vbox for proper installation
 sudo apt autoremove virtualbox-dkms
 
-# Install VirtualBox.
+# Install VirtualBox tools.
 sudo apt -y install build-essential linux-headers-`uname -r` dkms virtualbox-dkms
 
 # Install Vagrant.
-sudo apt -y install vagrant virtualbox
+sudo apt -y install vagrant
+
+# Install VirtualBox.
+sudo apt -y install virtualbox
 
 # Pre-accept virtualbox-ext-pack license.
 echo virtualbox-ext-pack virtualbox-ext-pack/license select true | sudo debconf-set-selections
