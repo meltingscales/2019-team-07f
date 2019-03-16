3abf44dfad4d0bf47416d4944042b402eef332c0
Henry Post <HenryFBP@gmail.com>
2019-03-16 11:11:15 -0500

    comment lines

--- ci-scripts/linux/install-deps.sh
+++ ci-scripts/linux/install-deps.sh
@@ -26,9 +26,14 @@ echo virtualbox-ext-pack virtualbox-ext-pack/license select true | sudo debconf-
 if [[ $? = 100 ]]; then
     echo "Error 100! Could not find virtualbox-ext-pack...Installing with curl."
 
+    # Filename of the extpack.
     VBOX_EXT_FILENAME=Oracle_VM_VirtualBox_Extension_Pack-6.0.4.vbox-extpack
+
+    # License key for unattended installations.
+    # This is not a security risk, just proof I've accepted the license once.
     LICENSE_KEY=56be48f923303c8cababb0bb4c478284b688ed23f16d775d729b89a2e8e5f9e
 
+    # If the extpack doesn't exist, then
     if [[ ! -f /tmp/${VBOX_EXT_FILENAME} ]]; then
         # Download it.
         curl https://download.virtualbox.org/virtualbox/6.0.4/${VBOX_EXT_FILENAME} --output /tmp/${VBOX_EXT_FILENAME}
