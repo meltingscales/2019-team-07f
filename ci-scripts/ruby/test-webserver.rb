20b68174c65b39e2b3835b49d3ac9705d2e3d92d
Henry Post <HenryFBP@gmail.com>
2019-03-19 00:17:52 -0500

    correctly use ruby's http connection library

--- ci-scripts/ruby/test-webserver.rb
+++ ci-scripts/ruby/test-webserver.rb
@@ -4,17 +4,24 @@ require_relative '../../__init__'
 
 VARIABLES = get_variables
 
-begin
-  Net::HTTP.start(VARIABLES['local']['host'], VARIABLES['local']['port']) do |http|
-    response = http.head('searchable-video-library')
+url = URI("http://#{VARIABLES['local']['host']}:#{VARIABLES['local']['port']}/#{VARIABLES['local']['uri']}")
 
-    puts response.code
-    if response.code != 200
-      puts response.body
-      puts response
-      raise "Didn't get 200 OK! Failure!"
-    end
-  end
-rescue Errno::ECONNREFUSED
-  raise "Couldn't connect to web server at #{VARIABLES['local']['host']}:#{VARIABLES['local']['port']}/#{VARIABLES['local']['uri']}! Failure!"
+string_resp = Net::HTTP.get(url)
+status_resp = Net::HTTP.get_response(url)
+
+
+  puts url
+puts string_resp
+puts status_resp
+puts status_resp.class
+
+case status_resp
+when Net::HTTPSuccess
+  puts("HTTP 2xx Success!")
+  exit(0)
+when Net::HTTPFound
+  puts("HTTP 302 Found (i.e. redirect)")
+  exit(0)
+else
+  raise("Not success or found...must be error! #{status_resp.class}")
 end
