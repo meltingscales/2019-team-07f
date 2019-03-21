ff7f1458dde8f2952e451b1b1245eb7e9bdf7f44
Henry Post <HenryFBP@gmail.com>
2019-03-20 21:44:17 -0500

    print for each install

--- ci-scripts/windows/install-deps.ps1
+++ ci-scripts/windows/install-deps.ps1
@@ -9,6 +9,13 @@ else
     Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 }
 
+Write-Host "Installing ruby."
 choco install -y ruby
+
+
+Write-Host "Installing virtualbox."
 choco install -y virtualbox
+
+
+Write-Host "Installing packer."
 choco install -y packer
