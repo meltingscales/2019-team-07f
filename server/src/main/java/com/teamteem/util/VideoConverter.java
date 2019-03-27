1a588026a041c808d26d3b1c73c7355d7445530f
Idris Fagbemi <idrisniyi94@gmail.com>
2019-03-27 14:55:20 -0500

    Fixed files

--- server/src/main/java/com/teamteem/util/VideoConverter.java
+++ server/src/main/java/com/teamteem/util/VideoConverter.java
@@ -1,5 +1,7 @@
 package com.teamteem.util;
 
+import java.util.Scanner;
+import java.io.File;
 import com.xuggle.mediatool.IMediaReader;
 import com.xuggle.mediatool.IMediaWriter;
 import com.xuggle.mediatool.ToolFactory;
@@ -11,8 +13,16 @@ class VideoToAudio{
 
 
     public void convertVideoToAudio(){
-        IMediaReader reader = ToolFactory.makeReader("C://Users//Administrator//Downloads//video.mp4");
-        IMediaWriter writer = ToolFactory.makeWriter("C://Users//Administrator//Downloads//video.mp3",reader);
+        Scanner s1=new Scanner(System.in);
+        System.out.print("Enter input video path: ");
+        String input=s1.next();
+        System.out.print("Enter output audio path: ");
+        String output=s1.next();
+        File source = new File(input);
+        File target = new File(output);
+
+        IMediaReader reader = ToolFactory.makeReader(input);
+        IMediaWriter writer = ToolFactory.makeWriter(output,reader);
 
         int sampleRate = 44100;
         int channels = 1;
