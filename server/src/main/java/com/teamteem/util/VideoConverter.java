e16bc3133180f3342b5283dd616de8ec6d642c4e
Idris Fagbemi <idrisniyi94@gmail.com>
2019-03-26 22:34:44 -0500

    added video converter file

--- server/src/main/java/com/teamteem/util/VideoConverter.java
+++ server/src/main/java/com/teamteem/util/VideoConverter.java
@@ -3,6 +3,11 @@ package com.teamteem.util;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.nio.file.Path;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public final class VideoConverter {
     /***
@@ -13,15 +18,32 @@ public final class VideoConverter {
      * @return A path to the converted MP3 file.
      * @throws FileNotFoundException Thrown when the video file doesn't exist.
      */
-    public static File convertVideo(File pathToVideo) throws FileNotFoundException {
+    private static final Logger LOG = Logger.getLogger(VideoConverter.class.getName());
 
-        // If video file does not exist,
-        if (!pathToVideo.exists()) {
-            throw new FileNotFoundException("Video file doesn't exist!");
+    public static void main(String[] args) {
+
+        try {
+            String line;
+            String mp4File = "tools/video-to-audio/sampledata/video/potato.mp4potato.mp4";
+            String mp3File = "tools/video-to-audio/sampledata/video/potato.mp3";
+
+            // ffmpeg -i input.mp4 output.avi as it's on www.ffmpeg.org
+//            FFmpeg ffmpeg = new FFmpeg("/path/to/ffmpeg");
+//            FFprobe ffprobe = new FFprobe("/path/to/ffprobe");
+            String cmd = "ffmpeg -i " + mp4File + " " + mp3File;
+            Process p = Runtime.getRuntime().exec(cmd);
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(p.getErrorStream()));
+            while ((line = in.readLine()) != null) {
+                System.out.println(line);
+            }
+            p.waitFor();
+            System.out.println("Video converted successfully!");
+            in.close();
+        } catch (IOException | InterruptedException e) {
+            LOG.log(Level.SEVERE, null, e);
         }
 
-        // TODO insert conversion code here...
-
-        return new File("not_yet_implemented.mp3");
     }
+
 }
