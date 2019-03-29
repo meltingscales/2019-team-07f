6245c233b36b6125ad17f91323a819f1bc98ff4c
Divin Gregis Baniekona <dbaniekona@hawk.iit.edu>
2019-03-29 11:30:51 -0500

    Add a backing bean class to handle the video files upload

---
+++ server/src/main/java/UploadBean.java
@@ -0,0 +1,47 @@
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
+import javax.servlet.http.Part;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Scanner;
+import java.io.IOException;
+
+@ManagedBean
+@SessionScoped
+public class UploadBean {
+
+    private Part file;
+    public String upload(){
+        try {
+            InputStream input=file.getInputStream();
+            File f=new File("C:\\Users\\Baniek\\2019-team-07f\\server\\src\\main\\java\\file.txt");
+            if(!f.exists()){
+                f.createNewFile();
+            }
+            FileOutputStream output=new FileOutputStream(f);
+            byte[] buffer=new byte[1024];
+            int length;
+            while((length=input.read(buffer))>0){
+                output.write(buffer, 0, length);
+            }
+            input.close();
+            output.close();
+        }catch(Exception e){
+            e.printStackTrace(System.out);
+        }
+        return "success";
+    }
+
+    public Part getFile() {
+        return file;
+    }
+
+    public void setFile(Part file) {
+        this.file = file;
+    }
+}
