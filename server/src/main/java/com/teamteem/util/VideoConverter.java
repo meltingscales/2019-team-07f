99b402115fef2af801bb57ba9d7e854e58374698
Idris Fagbemi <idrisniyi94@gmail.com>
2019-03-31 12:44:10 -0500

    modify file

--- server/src/main/java/com/teamteem/util/VideoConverter.java
+++ server/src/main/java/com/teamteem/util/VideoConverter.java
@@ -17,7 +17,7 @@ import java.util.logging.Logger;
  *
  */
 @ManagedBean(name = "videoconverter")
- class VideoConverter extends HttpServlet{
+ class VideoConverter implements javax.servlet.ServletContextListener{
 
     private static final Logger LOG = Logger.getLogger(VideoConverter.class.getName());
 
