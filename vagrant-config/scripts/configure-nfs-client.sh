fd7d50cd6919a7c4c6a94d1513436a67d5933ea5
HenryFBP <HenryFBP@gmail.com>
2019-04-12 14:56:19 -0500

    only mount nfs disk if not mounted

--- vagrant-config/scripts/configure-nfs-client.sh
+++ vagrant-config/scripts/configure-nfs-client.sh
@@ -2,8 +2,13 @@
 
 sudo apt-get install -y nfs-common
 
-if [[ ! -d $MOUNT_LOCATION ]]; then
-    sudo mkdir -p $MOUNT_LOCATION
+if [[ ! -d ${MOUNT_LOCATION} ]]; then
+    sudo mkdir -p ${MOUNT_LOCATION}
 fi
 
-sudo mount $NFS_SERVER_IP:/mnt/sharedfolder $MOUNT_LOCATION
+if mount | grep ${MOUNT_LOCATION} > /dev/null; then
+    echo "Already mounted ${MOUNT_LOCATION}. Not mounting again."
+else
+    echo "${MOUNT_LOCATION} is NOT mounted. Mounting..."
+    sudo mount ${NFS_SERVER_IP}:/mnt/sharedfolder ${MOUNT_LOCATION}
+fi
