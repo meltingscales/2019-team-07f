ec737eafca9f4a27b274c536136c8672e535b337
HenryFBP <HenryFBP@gmail.com>
2019-04-12 13:40:04 -0500

    remove CRLF from config file

--- vagrant-config/scripts/configure-nfs-server.sh
+++ vagrant-config/scripts/configure-nfs-server.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
 sudo apt install -y nfs-kernel-server
+sudo apt install -y dos2unix
 
 # Copy config file.
 sudo mv /tmp/exports /etc/exports
+
+# Remove CRLF from config file
+sudo dos2unix /etc/exports
 
 if [[ ! -d /mnt/sharedfolder ]]; then
     sudo mkdir -p /mnt/sharedfolder
