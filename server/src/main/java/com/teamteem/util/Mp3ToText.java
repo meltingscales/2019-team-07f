1dc0146078a4b627a4548918ee238e1783ab4f97
Divin Gregis Baniekona <dbaniekona@hawk.iit.edu>
2019-04-26 15:05:39 -0500

    change code to switch from google API to sphinx4

--- server/src/main/java/com/teamteem/util/Mp3ToText.java
+++ server/src/main/java/com/teamteem/util/Mp3ToText.java
@@ -1,55 +1,30 @@
 package com.teamteem.util;
-// Imports the Google Cloud client library
-import com.google.cloud.speech.v1.RecognitionAudio;
-import com.google.cloud.speech.v1.RecognitionConfig;
-import com.google.cloud.speech.v1.RecognitionConfig.AudioEncoding;
-import com.google.cloud.speech.v1.RecognizeResponse;
-import com.google.cloud.speech.v1.SpeechClient;
-import com.google.cloud.speech.v1.SpeechRecognitionAlternative;
-import com.google.cloud.speech.v1.SpeechRecognitionResult;
-import com.google.protobuf.ByteString;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 
+import edu.cmu.sphinx.api.Configuration;
+import edu.cmu.sphinx.api.SpeechResult;
+import edu.cmu.sphinx.api.StreamSpeechRecognizer;
 
 public class Mp3ToText {
-    /**
-     * Demonstrates using the Speech API to transcribe an audio file.
-     */
-    public static void main(String... args) throws Exception {
-        // Instantiates a client
-        try (SpeechClient speechClient = SpeechClient.create()) {
 
-            // The path to the audio file to transcribe
-            String fileName = "C:\\Temp\\Demo.mp3";
+    public static void main(String[] args) throws Exception {
 
-            // Reads the audio file into memory
-            Path path = Paths.get(fileName);
-            byte[] data = Files.readAllBytes(path);
-            ByteString audioBytes = ByteString.copyFrom(data);
+        Configuration configuration = new Configuration();
 
-            // Builds the sync recognize request
-            RecognitionConfig config = RecognitionConfig.newBuilder()
-                    .setEncoding(AudioEncoding.LINEAR16)
-                    .setSampleRateHertz(16000)
-                    .setLanguageCode("en-US")
-                    .build();
-            RecognitionAudio audio = RecognitionAudio.newBuilder()
-                    .setContent(audioBytes)
-                    .build();
+        configuration.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
+        configuration.setDictionaryPath("resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict");
+        configuration.setLanguageModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin");
 
-            // Performs speech recognition on the audio file
-            RecognizeResponse response = speechClient.recognize(config, audio);
-            List<SpeechRecognitionResult> results = response.getResultsList();
+        StreamSpeechRecognizer recognizer = new StreamSpeechRecognizer(configuration);
+        InputStream stream = new FileInputStream(new File("test.wav"));
 
-            for (SpeechRecognitionResult result : results) {
-                // There can be several alternative transcripts for a given chunk of speech. Just use the
-                // first (most likely) one here.
-                SpeechRecognitionAlternative alternative = result.getAlternativesList().get(0);
-                System.out.printf("Transcription: %s%n", alternative.getTranscript());
-            }
+        recognizer.startRecognition(stream);
+        SpeechResult result;
+        while ((result = recognizer.getResult()) != null) {
+            System.out.format("Hypothesis: %s\n", result.getHypothesis());
         }
+        recognizer.stopRecognition();
     }
 }
