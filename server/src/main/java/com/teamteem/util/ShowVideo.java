371dd8ea49a2f7ea3268d982b69f8c50f50df46b
Idris Fagbemi <idrisniyi94@gmail.com>
2019-04-29 22:56:52 -0500

    added new files

---
+++ server/src/main/java/com/teamteem/util/ShowVideo.java
@@ -0,0 +1,28 @@
+package com.teamteem.util;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class ShowVideo extends HttpServlet {
+
+
+    public void doGet(HttpServletRequest request,HttpServletResponse response)
+            throws IOException
+    {
+        response.setContentType("video/mp4");
+        ServletOutputStream out = response.getOutputStream();
+        FileInputStream fin = new FileInputStream("/mnt/nfs_videos/");
+
+        byte [] buf = new byte[4096];
+        int read;
+        while((read = fin.read(buf)) != -1)
+        {
+            out.write(buf, 0, read);
+        }
+
+        fin.close();
+        out.flush();
+        out.close();
+    }
+}
