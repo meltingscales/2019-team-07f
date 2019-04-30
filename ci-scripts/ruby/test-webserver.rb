f2eec9502605342702b3f4ad14686cefbf0a6bd0
Henry Post <HenryFBP@gmail.com>
2019-04-30 13:35:48 -0500

    break out testing code to own function and externalize schema to config vars

--- ci-scripts/ruby/test-webserver.rb
+++ ci-scripts/ruby/test-webserver.rb
@@ -2,26 +2,30 @@ require 'net/http'
 
 require_relative '../../__init__'
 
+def testResponse(response)
+  case response
+  when Net::HTTPSuccess
+    puts("HTTP 2xx Success!")
+    exit(0)
+  when Net::HTTPRedirection
+    puts("HTTP 302 Found (i.e. redirect).")
+    exit(0)
+  else
+    response.error!
+  end
+end
+
+
 VARIABLES = get_variables
 
-url = URI("http://#{VARIABLES['local']['host']}:#{VARIABLES['local']['port']}/#{VARIABLES['local']['uri']}")
+url = URI("#{VARIABLES['local']['scheme']}://#{VARIABLES['local']['host']}:#{VARIABLES['local']['port']}/#{VARIABLES['local']['uri']}")
 
 string_resp = Net::HTTP.get(url)
 status_resp = Net::HTTP.get_response(url)
 
-
-  puts url
+puts url
 puts string_resp
 puts status_resp
 puts status_resp.class
 
-case status_resp
-when Net::HTTPSuccess
-  puts("HTTP 2xx Success!")
-  exit(0)
-when Net::HTTPFound
-  puts("HTTP 302 Found (i.e. redirect)")
-  exit(0)
-else
-  raise("Not success or found...must be error! #{status_resp.class}")
-end
+testResponse(status_resp)
