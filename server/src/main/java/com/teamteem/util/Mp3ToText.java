3562fccd6d355982c0dafb2f575dcc5dc5a22b2e
YiTing7092 <ylin95@hawk.iit.edu>
2019-05-02 20:42:23 -0500

    using google api to convert mp3 to wav

--- server/src/main/java/com/teamteem/util/Mp3ToText.java
+++ server/src/main/java/com/teamteem/util/Mp3ToText.java
@@ -5,8 +5,10 @@ import edu.cmu.sphinx.api.SpeechResult;
 import edu.cmu.sphinx.api.StreamSpeechRecognizer;
 
 import javax.faces.bean.ManagedBean;
+import javax.sound.sampled.*;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 
 /* Imports the Google Cloud client library */
@@ -38,6 +40,24 @@ public class Mp3ToText {
             System.out.format("Hypothesis: %s\n", result.getHypothesis());
         }
         recognizer.stopRecognition();
+    }
+
+    public static void mp3ToWav(File mp3Data) throws UnsupportedAudioFileException, IOException {
+        // open stream
+        AudioInputStream mp3Stream = AudioSystem.getAudioInputStream(mp3Data);
+        AudioFormat sourceFormat = mp3Stream.getFormat();
+        // create audio format object for the desired stream/audio format
+        // this is *not* the same as the file format (wav)
+        AudioFormat convertFormat = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED,
+                sourceFormat.getSampleRate(), 16,
+                sourceFormat.getChannels(),
+                sourceFormat.getChannels() * 2,
+                sourceFormat.getSampleRate(),
+                false);
+        // create stream that delivers the desired format
+        AudioInputStream converted = AudioSystem.getAudioInputStream(convertFormat, mp3Stream);
+        // write stream into a file with file format wav
+        AudioSystem.write(converted, AudioFileFormat.Type.WAVE, new File("C:\\temp\\out.wav"));
     }
 
 }
